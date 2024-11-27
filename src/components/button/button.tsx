4e373661a4579dbb58cba601037d19cec87668a9
Guilherme Carvalho <guibrancopc@gmail.com>
2024-11-26 23:16:17 -0300

    feat: create button component

---
+++ src/components/button/button.tsx
@@ -0,0 +1,19 @@
+import { PropsWithChildren } from 'react';
+
+type Props = {
+  onClick: () => void;
+  kind?: 'primary' | 'secondary' | 'text';
+  disabled?: boolean;
+};
+
+export function Button({
+  children,
+  disabled,
+  onClick,
+}: PropsWithChildren<Props>) {
+  return (
+    <button className="c-button" disabled={disabled} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
