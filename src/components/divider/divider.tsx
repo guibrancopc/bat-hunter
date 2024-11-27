73d340781c14b693c631262fed6bd3afdc1fa0c3
Guilherme Carvalho <guibrancopc@gmail.com>
2024-11-26 23:16:54 -0300

    feat: create divider component

---
+++ src/components/divider/divider.tsx
@@ -0,0 +1,12 @@
+export const Divider = () => {
+  return (
+    <>
+      <div className="c-divider" />
+      <style>{`
+        .c-divider {
+          border: solid 1px black;
+        }
+      `}</style>
+    </>
+  );
+};
