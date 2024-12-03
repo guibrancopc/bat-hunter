802f08431d14c6d8794ff2e149b615ed083bdbe3
Guilherme Carvalho <guibrancopc@gmail.com>
2024-12-02 21:53:03 -0300

    feat: create button component kinds

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,19 +1,25 @@
 import { PropsWithChildren } from 'react';
 import './button.scss';
+import clsx from 'clsx';
 
 type Props = {
   onClick: () => void;
-  kind?: 'primary' | 'secondary' | 'text';
+  kind?: 'primary' | 'secondary' | 'tertiary';
   disabled?: boolean;
 };
 
 export function Button({
+  kind = 'secondary',
   children,
   disabled,
   onClick,
 }: PropsWithChildren<Props>) {
   return (
-    <button className="bh-button" disabled={disabled} onClick={onClick}>
+    <button
+      className={clsx('bh-button', `bh-button--${kind}`)}
+      disabled={disabled}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
