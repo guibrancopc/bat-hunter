479cb5dfd7a01ceabc6364febf7ee836655074c1
Guilherme Carvalho <guibrancopc@gmail.com>
2025-01-09 22:34:20 -0300

    feat: add class name prop on text component

--- src/components/text/text.tsx
+++ src/components/text/text.tsx
@@ -5,14 +5,17 @@ import './text.scss';
 export function Text({
   size = 'md',
   weight = 'light',
+  className,
   children,
 }: PropsWithChildren<{
+  className?: string;
   size?: 'sm' | 'md' | 'lg' | 'xl';
   weight?: 'light' | 'medium' | 'bold';
 }>) {
   return (
     <span
       className={clsx(
+        className,
         'bh-text',
         `bh-text__size--${size}`,
         `bh-text__weight--${weight}`
