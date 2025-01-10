5d8a0293e6683b718cd97645b745514a93feedf1
Guilherme Carvalho <guibrancopc@gmail.com>
2025-01-09 22:27:38 -0300

    fix: update components/index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,6 @@ export * from './button';
 export * from './button-group';
 export * from './card';
 export * from './divider';
+export * from './gutter';
+export * from './title';
+export * from './text';
