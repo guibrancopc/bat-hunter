841e72dda895cf0d60790daf7db57469335b79af
Guilherme Carvalho <guibrancopc@gmail.com>
2025-05-07 21:12:53 -0300

    feat: add fade-in effect in home page bg image

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,17 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router';
 import { Button } from 'src/components';
 import { disableBatGame } from 'src/services/game-service';
 import './home-page.scss';
+import clsx from 'clsx';
 
 export function HomePage() {
+  const [showBg, setShowBg] = useState(false);
+
   useEffect(() => {
     disableBatGame();
+    setTimeout(() => setShowBg(true), 100);
   }, []);
 
   return (
     <div className="home-page">
-      <img className="home-page-bg" src="/images/sniper-aim-bg.jpg" />
+      <img
+        className={clsx('home-page-bg', { visible: showBg })}
+        src="/images/sniper-aim-bg.jpg"
+      />
       <div className="box">
         <div className="text">Too many bats, time to drop some.</div>
         <br />
