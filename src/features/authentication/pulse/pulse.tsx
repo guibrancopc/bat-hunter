27de4da362e4d4473a373d2b05b3f18c3fa65134
Guilherme Carvalho <guibrancopc@gmail.com>
2026-01-04 23:02:33 -0300

    refactor: pulse logic to use click events

--- src/features/authentication/pulse/pulse.tsx
+++ src/features/authentication/pulse/pulse.tsx
@@ -1,20 +1,45 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuthContext } from '../auth-context';
 import { isLoggedIn } from 'src/services/authentication-service';
 
+// @TODO: remove the pulse logic and replace by another strategy to identify idle player
 export function Pulse() {
-  const TEN_SECONDS = 30 * 1000;
+  const THIRDY_SECONDS = 30 * 1000;
+  const [lastPulseAt, setLastPulseAt] = useState(0);
   const { setCurrentUser } = useAuthContext();
 
   useEffect(() => {
-    setInterval(() => {
-      if (isLoggedIn()) {
-        setCurrentUser({
-          lastPulseAt: Date.now(),
-        });
-      }
-    }, TEN_SECONDS);
+    debounce(() => {
+      onEveryUserClick(() => {
+        if (!isLoggedIn()) return;
+
+        console.log('ON_CLICK');
+        const now = Date.now();
+
+        if (now > lastPulseAt + THIRDY_SECONDS) {
+          setLastPulseAt(now);
+          setCurrentUser({ lastPulseAt: now });
+        }
+      });
+    });
   }, []);
 
   return <></>;
+}
+
+// @TODO: Any better way of implementing the debounce?
+let runToggle = true;
+function debounce(cb: () => void, wait = 1000) {
+  if (runToggle) {
+    cb?.();
+    runToggle = false;
+  }
+
+  setTimeout(() => {
+    runToggle = true;
+  }, 5 * 1000);
+}
+
+function onEveryUserClick(cb: () => void) {
+  window.document.addEventListener('click', cb);
 }
