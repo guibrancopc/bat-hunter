d01d8ec8a9a2ee7dd68ffcc2bf4407fca3f002e2
Guilherme Carvalho <guibrancopc@gmail.com>
2026-01-05 21:49:06 -0300

    fix: pulse logic

--- src/features/authentication/pulse/pulse.tsx
+++ src/features/authentication/pulse/pulse.tsx
@@ -1,43 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useAuthContext } from '../auth-context';
 import { isLoggedIn } from 'src/services/authentication-service';
+import { debounce } from 'src/services/debounce-service';
 
-// @TODO: remove the pulse logic and replace by another strategy to identify idle player
+let lastPulseAt = 0;
+const DELAY_IN_SECONDS = 20 * 1000;
+
 export function Pulse() {
-  const THIRDY_SECONDS = 30 * 1000;
-  const [lastPulseAt, setLastPulseAt] = useState(0);
   const { setCurrentUser } = useAuthContext();
 
   useEffect(() => {
-    debounce(() => {
+    debounce(() =>
       onEveryUserClick(() => {
         if (!isLoggedIn()) return;
 
-        console.log('ON_CLICK');
         const now = Date.now();
 
-        if (now > lastPulseAt + THIRDY_SECONDS) {
-          setLastPulseAt(now);
+        if (now > lastPulseAt + DELAY_IN_SECONDS) {
+          lastPulseAt = now;
           setCurrentUser({ lastPulseAt: now });
         }
-      });
-    });
+      })
+    );
   }, []);
 
   return <></>;
-}
-
-// @TODO: Any better way of implementing the debounce?
-let runToggle = true;
-function debounce(cb: () => void, wait = 1000) {
-  if (runToggle) {
-    cb?.();
-    runToggle = false;
-  }
-
-  setTimeout(() => {
-    runToggle = true;
-  }, 5 * 1000);
 }
 
 function onEveryUserClick(cb: () => void) {
